8c3a306c74542b53c17b0c7aecd7d5ad858f9475
Tyler Brown <tyler.brown@uconn.edu>
2025-08-21 14:59:18 -0400

    delete user, delete post, change with id/username resources

--- backend/server.py
+++ backend/server.py
@@ -44,7 +44,7 @@ def get_users():
     return jsonify({'users': list(map(parse_user, data))}), 200
 
 # Get user by username
-@app.route('/api/user/<username>', methods=['GET'])
+@app.route('/api/user/username/<username>', methods=['GET'])
 def get_user(username):
     username = username
     cursor = conn.cursor()
@@ -52,6 +52,21 @@ def get_user(username):
         SELECT id, username, created_at FROM users
         WHERE username = %s
     """, (username,))
+    res = cursor.fetchone()
+    cursor.close()
+    if res is None:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify({"user": parse_user(res)}), 200
+
+# Get user by username
+@app.route('/api/user/<int:user_id>', methods=['GET'])
+def get_user_id(user_id):
+    user_id = user_id
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT id, username, created_at FROM users
+        WHERE id = %s
+    """, (user_id,))
     res = cursor.fetchone()
     cursor.close()
     if res is None:
@@ -80,6 +95,16 @@ def add_user():
     finally:
         cursor.close()
 
+@app.route('/api/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
+    if cursor.rowcount == 0:
+        cursor.close()
+        return jsonify({"error": "Post not found"}), 404
+    cursor.close()
+    return jsonify({"msg": f"User #{user_id} deleted"}), 200
+
 # Get post by ID
 @app.route('/api/post/<int:post_id>', methods=['GET'])
 def get_post(post_id):
@@ -95,6 +120,16 @@ def get_post(post_id):
         return jsonify({"error": "User not found"}), 404
     return jsonify({'post': parse_post(res)}), 200
 
+@app.route('/api/post/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM posts WHERE id = %s", (post_id,))
+    if cursor.rowcount == 0:
+        cursor.close()
+        return jsonify({"error": "Post not found"}), 404
+    cursor.close()
+    return jsonify({"msg": f"Post #{post_id} deleted"}), 200
+    
 # Get all posts using pagination
 @app.route('/api/feed/new', methods=['GET'])
 def get_new():
@@ -104,7 +139,7 @@ def get_new():
     cursor = conn.cursor()
     cursor.execute("""
         SELECT posts.id, user_id, content, image_url, posts.created_at, username FROM posts
-        JOIN users ON posts.user_id = users.id
+        LEFT JOIN users ON posts.user_id = users.id
         ORDER BY created_at DESC
         LiMIT %s OFFSET %s;
     """, (limit, offset))
@@ -134,7 +169,7 @@ def add_post():
         return jsonify({"error": f"user_id does not exist"}), 400
     finally:
         cursor.close()
-        
+    
 if __name__ == "__main__":
     try:
         app.run(host="0.0.0.0", port=5001, debug=True)
